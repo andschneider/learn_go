cebb3dec51205527fadf504cf79a0a36d5cdb64c
Andrew Schneider <aschne93@gmail.com>
2020-04-25 15:41:43 -0700

    Starting gocli ch2
    Basic ToDo CLI functionality and tests

---
+++ gocli/ch2/cmd/todo/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/andschneider/gocli/ch2/todo"
+)
+
+// Hardcoding the file name
+const todoFileName = ".todo.json"
+
+func main() {
+	// Display custom message for CLI
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"%s tool. Developed for The Pragmatic Bookshelf\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2020\n")
+		fmt.Fprintln(flag.CommandLine.Output(), "Usage information:")
+		flag.PrintDefaults()
+	}
+	// Parsing command line flags
+	task := flag.String("task", "", "Task to be included in the ToDo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+	flag.Parse()
+
+	l := &todo.List{}
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch {
+	case *list:
+		for _, item := range *l {
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
+		}
+	case *complete > 0:
+		// Complete the given item
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *task != " ":
+		l.Add(*task)
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
+	}
+}
---
+++ gocli/ch2/go.mod
@@ -0,0 +1,3 @@
+module github.com/andschneider/gocli/ch2/todo
+
+go 1.13
