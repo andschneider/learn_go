d9d857a47681173e660d9076d68645cbd63179a0
Andrew Schneider <aschne93@gmail.com>
2020-08-02 22:49:21 -0700

    gocli: Finish ch4
    haven't done any of the suggested exercises yet

--- gocli/ch4/walk/main.go
+++ gocli/ch4/walk/main.go
@@ -4,18 +4,22 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 )
 
 type config struct {
-	ext  string // extension to filter out
-	size int64  // min file size
-	list bool   // list files
-	del  bool   // delete files
+	ext     string    // extension to filter out
+	size    int64     // min file size
+	list    bool      // list files
+	del     bool      // delete files
+	wLog    io.Writer // log destination writer
+	archive string    // archive directory
 }
 
 func run(root string, out io.Writer, cfg config) error {
+	delLogger := log.New(cfg.wLog, "DELETED FILE: ", log.LstdFlags)
 	return filepath.Walk(root,
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
@@ -31,9 +35,15 @@ func run(root string, out io.Writer, cfg config) error {
 				return listFile(path, out)
 			}
 
+			if cfg.archive != "" {
+				if err := archiveFile(cfg.archive, root, path); err != nil {
+					return err
+				}
+			}
+
 			// Delete files
 			if cfg.del {
-				return delFile(path)
+				return delFile(path, delLogger)
 			}
 
 			// List is the default option if nothing else was set
@@ -43,17 +53,35 @@ func run(root string, out io.Writer, cfg config) error {
 
 func main() {
 	root := flag.String("root", ".", "Root directory to start")
+	logFile := flag.String("log", "", "Log deleted files to this file")
 	list := flag.Bool("list", false, "List files only")
+	archive := flag.String("archive", "", "Archive directory")
 	del := flag.Bool("del", false, "Delete files")
 	ext := flag.String("ext", "", "File extension to filter out")
 	size := flag.Int64("size", 0, "Minimum file size")
 	flag.Parse()
 
+	var (
+		f   = os.Stdout
+		err error
+	)
+
+	if *logFile != "" {
+		f, err = os.OpenFile(*logFile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+
 	c := config{
-		ext:  *ext,
-		size: *size,
-		list: *list,
-		del:  *del,
+		ext:     *ext,
+		size:    *size,
+		list:    *list,
+		del:     *del,
+		wLog:    f,
+		archive: *archive,
 	}
 
 	if err := run(*root, os.Stdout, c); err != nil {
