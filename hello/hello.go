514019cfe4daf4efdfcb136f9a2c452aebbc5b5b
Andrew Schneider <aschne93@gmail.com>
2020-08-06 09:21:40 -0700

    Remove newline in Println

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/andschneider/learn_go/stringutil"
 )
 
 func main() {
-	fmt.Println("hello world\n")
+	fmt.Println("hello world")
 	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
